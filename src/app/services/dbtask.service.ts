aebfbe345c10e5369a5133f379fe00b07274e903
jorge-a11-rgb <jorgeandresalfarocastro@gmail.com>
2021-10-28 07:49:26 -0300

    codigo innsert en registro hecho

--- src/app/services/dbtask.service.ts
+++ src/app/services/dbtask.service.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/semi */
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable @typescript-eslint/member-ordering */
 /* eslint-disable arrow-body-style */
 /* eslint-disable prefer-const */
 import { Injectable } from '@angular/core';
@@ -34,65 +37,53 @@ export class DBTaskService {
     (
       user_name TEXT PRIMARY KEY NOT NULL,
       password INTEGER NOT NULL,
+      password2 INTEGER NOT NULL,
       segundo_apellido_materno NOT NULL,
       active INTEGER(1) NOT NULL
     );
     `;
     return this.db.executeSql(tables);
   }
-  /**
-   * Retorna si existe un usuario activo o no.
-   */
-  sesionActive() {
-    alert('sesionActive');
 
+  sesionActive(){
     // Se desarrolla la consulta
     let sql = `SELECT user_name,active FROM sesion_data WHERE active=1 LIMIT 1`;
     // Se ejecuta la consulta y no le pasamos parametros [value,value1,...]
-    return (
-      this.db
-        .executeSql(sql, [])
-        // Cuando se ejecute la consulta
-        .then((response) => {
-          // obtenemos lo que devuelve la consulta
-          return Promise.resolve(response.rows.item(0)); // Se obtiene el primer item de la consulta y se retorna
-        })
-    );
+    return this.db.executeSql(sql,[])
+    // Cuando se ejecute la consulta
+    .then(response=>{ // obtenemos lo que devuelve la consulta
+      return Promise.resolve(response.rows.item(0)); // Se obtiene el primer item de la consulta y se retorna
+    });
   }
   /*** Función que valida la existencia del usuario que esta iniciando sesión
    * @param sesion Datos de inicio de sesión Usuario y Password
    */
-  getSesionData(sesion: any) {
-    alert('getSesionData');
-
+  getSesionData(sesion: any){
     let sql = `SELECT user_name, active FROM sesion_data
     WHERE user_name=? AND password=? LIMIT 1`;
-    return this.db
-      .executeSql(sql, [sesion.Usuario, sesion.Password])
-      .then((response) => {
+    return this.db.executeSql(sql,[sesion.Usuario,
+      sesion.Password]).then(response=>{
         return Promise.resolve(response.rows.item(0));
       });
   }
   /*** Función que crea un nuevo registro de inicio de sesión
    * @param sesion Datos de inicio de sesión Usuario, Password y Active
    */
-  createSesionData(sesion: any) {
-    alert('createSesionData');
-
-    let sql = `INSERT INTO sesion_data(user_name,password,active)
-    VALUES(?,?,?)`;
-    return this.db
-      .executeSql(sql, [sesion.Usuario, sesion.Password, sesion.Active])
-      .then((response) => {
+  createSesionData(sesion: any){
+    let sql = `INSERT INTO sesion_data(user_name,password,password2,segundo_apellido_materno,active)
+    VALUES(?,?,?,?)`;
+    return this.db.executeSql(sql, [sesion.Usuario,
+      sesion.Password, sesion.Active]).then(response=>{
         return Promise.resolve(response.rows.item(0));
-      });
+      });;
   }
-  updateSesionData(sesion: any) {
-    alert('updateSesionData');
-
+  updateSesionData(sesion: any){
     let sql = `UPDATE sesion_data
     SET active=?
     WHERE user_name=?`;
-    return this.db.executeSql(sql, [sesion.active, sesion.user_name]);
+    return this.db.executeSql(sql, [sesion.active,sesion.user_name]);
   }
+
 }
+
+
